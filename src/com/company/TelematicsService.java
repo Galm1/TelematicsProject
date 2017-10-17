6f691dab1d6a81fc734fea8382d405c334a868ff
EliGarcia <garciaelisha93@yahoo.com>
2017-10-16 19:42:38 -0500

    find the file and turn it into an object. then turn it into a json file

--- src/com/company/TelematicsService.java
+++ src/com/company/TelematicsService.java
@@ -3,8 +3,10 @@ package com.company;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 
 
@@ -26,6 +28,25 @@ public class TelematicsService {
             fileWriter.close();
         } catch (IOException ex) {
             ex.printStackTrace();
+        }
+
+        File file = new File(".");
+
+        ArrayList<VehicleInfo> vehicles = new ArrayList<>();
+
+        for (File f : file.listFiles()) {
+            if (f.getName().endsWith(".json")) {
+                try {
+                    ObjectMapper mapper = new ObjectMapper();
+                    VehicleInfo vi = mapper.readValue(f, VehicleInfo.class);
+                    vehicles.add(vi);
+                } catch (FileNotFoundException ex) {
+                    System.out.println("Could not find file *" + f + "*");
+                    ex.printStackTrace();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
         }
     }
 }
